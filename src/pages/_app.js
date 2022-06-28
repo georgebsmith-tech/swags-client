ef663a4b42d9c26851768348a9c3c9751f0f0600
georgebsmith-tech <georgebsmith.tech@gmail.com>
2022-06-28 23:44:31 +0100

    added more

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,7 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <h1>This is for all pages</h1>
+      <h1>This is for all pages and more</h1>
       <Component {...pageProps} />
     </div>
   );
